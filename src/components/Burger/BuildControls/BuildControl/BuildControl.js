44703bde6f5d818be8400ded9e856ab1d00c38ff
DESKTOP-FICELG4\knigh <dogukanacet@gmail.com>
2021-01-15 15:40:28 +0300

    no message

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./BuildControl.module.css";
+
+const buildControl = (props) => (
+  <div className={styles.BuildControl}>
+    <div className={styles.Label}>{props.label}</div>
+    <button
+      onClick={props.removed}
+      disabled={props.disable}
+      className={styles.Less}
+    >
+      Less
+    </button>
+    <button className={styles.More} onClick={props.added}>
+      More
+    </button>
+  </div>
+);
+
+export default buildControl;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,31 @@ import styles from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients);
+  console.log(transformedIngredients);
+
+  transformedIngredients = transformedIngredients.map((igKey) => {
+    return [...Array(props.ingredients[igKey])] // [ , ]
+      .map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+  });
+
+  console.log(transformedIngredients);
+
+  transformedIngredients = transformedIngredients.reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+
+  console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>add ingredients</p>;
+  }
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
