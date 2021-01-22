48d2b03f2d767e88fec619b39c0cb71a6d02600e
DESKTOP-FICELG4\knigh <dogukanacet@gmail.com>
2021-01-22 12:27:43 +0300

    no message

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import styles from "./Layout.module.css";
 
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+
 const layout = (props) => (
   <Fragment>
-    <div>toolbar, sidedrawer, backdrop</div>
+    <Toolbar />
     <main className={styles.Content}>{props.children}</main>
   </Fragment>
 );
